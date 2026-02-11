b8b791757c581404db6497911f1a7a9823787d38
TheFerrari <alejandro.chivas132@hotmail.com>
2026-02-11 08:16:39 -0800

    Minimal changes on launch.bat

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 python -m venv venv
 venv\Scripts\pip.exe install -r requirements.txt
 
-REM venv\Scripts\python.exe core/setup_generate.py
+venv\Scripts\python.exe core/setup_generate.py
 venv\Scripts\python.exe aiya.py
